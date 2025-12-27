ed9928adaf30903e0b9594874c85774fe696ba13
bubakrojek <jakub.brojek@interia.pl>
2025-12-27 18:46:40 +0100

    foodlog.py tests created

---
+++ domain/food_log.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import User
+
+from recipes.models import Recipe, FoodLog
+
+def val(custom,original):
+    return custom if custom is not None else original
+
+
+def get_entry_macros(food_log: FoodLog) -> dict:
+    if food_log.recipe is None and food_log.custom_title is None and food_log.custom_fat is None and food_log.custom_calories is None and food_log.custom_protein is None and food_log.custom_carbohydrates:
+        return {
+            'calories': 0,
+            'protein': 0,
+            'fat': 0,
+            'carbohydrates': 0
+        }
+    else:
+        return {
+            'calories': food_log.servings * (
+                food_log.custom_calories if food_log.custom_calories is not None else food_log.recipe.calories),
+            'protein': food_log.servings * (
+                food_log.custom_protein if food_log.custom_protein is not None else food_log.recipe.protein),
+            'fat': food_log.servings * (
+                food_log.custom_fat if food_log.custom_fat is not None else food_log.recipe.fat),
+            'carbohydrates': food_log.servings * (
+                food_log.custom_carbohydrates if food_log.custom_carbohydrates is not None else food_log.recipe.carbohydrates)
+        }
+
+def get_day_macros(user:User,date) -> dict:
+    food_logs=FoodLog.objects.filter(user=user,date__date=date)
+
+    return {
+        'calories':sum(get_entry_macros(log)['calories'] for log in food_logs),
+        'protein': sum(get_entry_macros(log)['protein'] for log in food_logs),
+        'fat': sum(get_entry_macros(log)['fat'] for log in food_logs),
+        'carbohydrates': sum(get_entry_macros(log)['carbohydrates'] for log in food_logs)
+    }
+
+def get_certain_food_log(user:User, date, meal_type:FoodLog.meal_type):
+    food_logs=FoodLog.objects.filter(user=user,date__date=date,meal_type=meal_type).select_related('recipe')
+    return [
+        {
+            'id':log.id,
+            'recipe_title':val(log.custom_title,log.recipe.title),
+            'serving':log.servings,
+            'macroelement':get_entry_macros(log),
+        }
+        for log in food_logs
+    ]
--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,97 @@
 from django.test import TestCase
-
+from domain.food_log import *
+from recipes.models import *
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class GetEntryMacrosTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user('test','test@test.com','1234')
+        cls.recipe=Recipe.objects.create(
+            title='Pizza',
+            calories=600, protein=100, carbohydrates=45, fat=10,
+            servings=2,
+            created_by=cls.user,
+        )
+
+    def test_use_recipe_value_when_no_custom(self):
+        entry=FoodLog.objects.create(
+            user=self.user,
+            meal_type=MealType.BREAKFAST,
+            recipe=self.recipe,
+            servings=3,
+        )
+        macros=get_entry_macros(entry)
+        self.assertEqual(macros['calories'],3*600)
+
+
+    def test_use_recipe_value_when_custom_present(self):
+        entry=FoodLog.objects.create(
+            user=self.user,
+            meal_type=MealType.DINNER,
+            recipe=self.recipe,
+            custom_calories=500,
+            servings=3,
+        )
+        macros=get_entry_macros(entry)
+        self.assertEqual(macros['calories'],3*500)
+
+    def test_get_certain_food_log(self):
+        entry = FoodLog.objects.create(
+            user=self.user,
+            meal_type=MealType.BREAKFAST,
+            recipe=self.recipe,
+            servings=3,
+        )
+        entry = FoodLog.objects.create(
+            user=self.user,
+            meal_type=MealType.BREAKFAST,
+            recipe=self.recipe,
+            servings=1,
+            custom_calories=500,
+            custom_title='PICCA'
+        )
+        from datetime import date
+        results=get_certain_food_log(user=self.user,date=date.today(),meal_type=MealType.BREAKFAST)
+        self.assertEqual(results[0]['macroelement']['calories'],3*600)
+        self.assertEqual(results[1]['macroelement']['calories'], 1 * 500)
+        self.assertEqual(results[1]['recipe_title'],'PICCA')
+
+class GetDayMacrosTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user=User.objects.create_user('test2','test2@test.com','12345')
+        cls.recipe=Recipe.objects.create(
+            title='Pizza',
+            calories=600, protein=100, carbohydrates=45, fat=10,
+            servings=2,
+            created_by=cls.user,
+        )
+
+        FoodLog.objects.create(
+            user=cls.user,
+            meal_type=MealType.SUPPER,
+            recipe=cls.recipe,
+            servings=2,
+        )
+        FoodLog.objects.create(
+            user=cls.user,
+            meal_type=MealType.BREAKFAST,
+            recipe=cls.recipe,
+            custom_calories=200,
+            servings=1,
+        )
+        FoodLog.objects.create(
+            user=cls.user,
+            meal_type=MealType.BREAKFAST,
+            recipe=cls.recipe,
+            custom_calories=300,
+            servings=2,
+        )
+
+    def test_get_day_macros(self):
+        from domain.food_log import get_day_macros
+        from datetime import date
+        macros=get_day_macros(self.user,date=date.today())
+        self.assertEqual(macros['calories'],2*600+1*200+2*300)
