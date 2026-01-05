463063ec8b12d53be8f99de87475a8b2ecfc0e75
bubakrojek <jakub.brojek@interia.pl>
2026-01-05 19:58:19 +0100

    Make weight log date unique

--- users/models.py
+++ users/models.py
@@ -26,7 +26,7 @@ class DietaryPreferences(models.Model):
 
 class WeightLog(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE, related_name='weight_logs')
-    date=models.DateField()
+    date=models.DateField(unique=True)
     weight=models.DecimalField(max_digits=5,decimal_places=2)
     notes=models.TextField(blank=True)
 
